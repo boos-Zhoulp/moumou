e0d1606b3dcfa9de2bde26126e6a12f3853ed6ac
Mr.zhou <78061245+boos-Zhoulp@users.noreply.github.com>
2023-10-26 20:06:40 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,7 +24,7 @@ module.exports = async function createConfigAsync() {
       metadata: [
         {
           name: 'keywords',
-          content: '愧怍, kuizuo',
+          content: '某某, moumou',
         },
         {
           name: 'keywords',
